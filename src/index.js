a0000b092eca5d80724f643d0b4372b4625e26fe
Бочманов Владислав <vladislavbochmanov@gmail.com>
2022-12-29 16:47:59 +0300

    feat: base html and style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import './style.css';
-import Icon from './assets/frankenstein.png';
-
-function createApp() {
-  const app = document.getElementById('app');
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  const p = document.createElement('p');
-  p.textContent = 'Hello, me!';
-
-  app.appendChild(myIcon);
-  app.appendChild(p);
-}
-
-createApp();
+import './css/style.css';
