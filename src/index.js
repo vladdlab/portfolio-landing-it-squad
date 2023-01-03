47e803be186a2743e98a0a3d21b654592cac19c9
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-03 08:28:29 +0300

    feat: base mobile menu styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './css/style.css';
+
+const mainNav = document.querySelector('.main-nav');
+const mainNavToggle = document.querySelector('.main-nav__toggle');
+mainNavToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('main-nav--closed');
+  mainNav.classList.toggle('main-nav--opened');
+})
