f2fc6a2f8226b162b33b242e83995defe3a0382a
Бочманов Владислав <vladislavbochmanov@gmail.com>
2023-01-06 13:09:22 +0300

    feat: mobile navigation

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import './css/style.css';
 
 const mainNav = document.querySelector('.main-nav');
 const mainNavToggle = document.querySelector('.main-nav__toggle');
-mainNavToggle.addEventListener('click', () => {
-  mainNav.classList.toggle('main-nav--closed');
-  mainNav.classList.toggle('main-nav--opened');
+const siteListItemsLinks = document.querySelectorAll('.site-list__item a')
+
+let mobileMenuOpened = false;
+let lastScroll = 0;
+function toggleMenu() {
+  if (document.documentElement.clientWidth >= 1200) return
+
+  if (mobileMenuOpened) {
+    mobileMenuOpened = false
+    document.body.classList.remove('page__body--nav-opened');
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+    document.body.style.top = '';
+    window.scrollTo(0, lastScroll);
+  } else {
+    mobileMenuOpened = true
+    lastScroll = window.scrollY;
+    mainNav.classList.add('main-nav--opened');
+    mainNav.classList.remove('main-nav--closed');
+    document.body.classList.add('page__body--nav-opened');
+
+
+  }
+}
+
+mainNavToggle.addEventListener('click', toggleMenu)
+siteListItemsLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu)
 })
